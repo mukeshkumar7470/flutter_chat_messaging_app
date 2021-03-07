eaf3a41240aef00567e523d558105a7460485d45
pc <kumarmukeshpatel57@gmail.com>
2021-03-07 23:51:07 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_messaging_app/screens/welcome/welcome_screen.dart';
+import 'package:flutter_chat_messaging_app/theme.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: lightThemeData(context),
+      darkTheme: darkThemeData(context),
+      home: WelcomeScreen(),
+    );
+  }
+}
